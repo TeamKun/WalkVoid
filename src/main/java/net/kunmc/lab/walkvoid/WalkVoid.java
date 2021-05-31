4eeb855ca46eb1f759fc3ab3bfdbea2ddbc6cd4e
Kei <69491052+Kei-7777@users.noreply.github.com>
2021-05-31 18:44:43 +0900

    Update WalkVoid.java

--- src/main/java/net/kunmc/lab/walkvoid/WalkVoid.java
+++ src/main/java/net/kunmc/lab/walkvoid/WalkVoid.java
@@ -4,15 +4,9 @@ import net.kunmc.lab.walkvoid.listener.WalkListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WalkVoid extends JavaPlugin {
-
     @Override
     public void onEnable() {
         Kei.out(Kei.a(this));
         Kei.a(new WalkListener(), this);
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     }
 }
